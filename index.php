87ac943cfd5f4e18e11fd6c14bccfa31f04bbd7f
marcos123trab <74323434+marcos123trab@users.noreply.github.com>
2020-11-13 16:35:05 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myfile = fopen("tr.txt", "a") or die("Unable to open file!");
-
-$log=$_GET['evento'].','.$_GET['descricao'].','.$_GET['data'].','.$_GET['identificador']."\n";
-
-fwrite($myfile, $log);
-echo 'Log enviado com sucesso';
-?>
