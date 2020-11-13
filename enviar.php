ddcff70ca1e96301584c0ccc35e609946778efc6
marcos123trab <74323434+marcos123trab@users.noreply.github.com>
2020-11-13 16:35:21 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,9 @@
+<?php
+$myfile = fopen("tr.txt", "a") or die("Unable to open file!");
+
+$log=$_GET['evento'].','.$_GET['descricao'].','.$_GET['data'].','.$_GET['identificador']."\n";
+
+fwrite($myfile, $log);
+echo 'Log enviado com sucesso';
+fclose($myfile);
+?>
